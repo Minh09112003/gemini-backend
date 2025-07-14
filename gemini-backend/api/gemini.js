ed34f8f8427a9c599393463f9c4c01346c928064
Minh09112003 <124022315+Minh09112003@users.noreply.github.com>
2025-07-14 20:08:32 +0700

    Update gemini.js

--- gemini-backend/api/gemini.js
+++ gemini-backend/api/gemini.js
@@ -1,7 +1,7 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const API_KEY = process.env.GEMINI_API_KEY;
-const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
+const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key=${API_KEY}`;
 
 module.exports = async (req, res) => {
   if (req.method !== "POST") {
